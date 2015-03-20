9666489fdbe05efd87721a0f8d10e4afc83da2f7
Sam <ItsMeSam@users.noreply.github.com>
2015-03-20 16:31:00 +0100

    :package: Main files

--- example/index.php
+++ example/index.php
@@ -1,12 +1,3 @@
-# RegTPL
-A super simple template engine, which was developed for Acuney Framework.
-
-# MVC
-Are you looking for a MVC example with RegTPL? Check my Acuney Framework repository.
-
-# Example
-
-```php
 <?php
 
 include "vendor/RegTPL.class.php"; //.. Include RegTPl
@@ -23,5 +14,3 @@ $tpl->parse('home'); //.. Parse the template file
 $tpl->display('home.tpl'); //.. Display the file
 
 ?>
-```
-This is a snippet from the example directory. Look in the example directory for a more wide example.
